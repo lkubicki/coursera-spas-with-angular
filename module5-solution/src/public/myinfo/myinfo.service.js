d289ae2bf09a3fb1b37ce47158b770e84ee93bae
lkubicki <lmkubicki@gmail.com>
2016-11-05 00:16:37 +0100

    Development

---
+++ module5-solution/src/public/myinfo/myinfo.service.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    angular.module('public')
+        .service('myinfoService', myinfoService);
+
+    function myinfoService() {
+        this.firstName = '';
+        this.lastName = '';
+        this.email = '';
+        this.phone = '';
+        this.favoriteDish = '';
+
+        this.setUserInfo = function (firstName, lstaNem,email, phone,favouriteDish) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.favoriteDish = favoriteDish;
+            this.phone = phone;
+        };
+
+        this.getUserInfo = function () {
+            return {
+                firstName: this.firstName,
+                lastName: this.lastName,
+                email: this.email,
+                favoriteDish: this.favoriteDish,
+                phone: this.phone
+            };
+        }
+    }
+})();
--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -3,20 +3,27 @@
     angular.module('public')
         .controller('signUpController', signUpController);
 
-    signUpController.$inject = ['MenuService', '$q'];
-    function signUpController(MenuService, $q) {
+    signUpController.$inject = ['MenuService', 'myinfoService'];
+    function signUpController(MenuService, myinfoService) {
+        var dishExists = true;
         var signUpCtrl = this;
         signUpCtrl.menuService = MenuService;
+        signUpCtrl.myinfoSrvc = myinfoService;
 
         signUpCtrl.submit = function () {
-            signUpCtrl.completed = true;
+            signUpCtrl.myinfoSrvc.setUserInfo(signUpCtrl.user.firstName, signUpCtrl.user.lastName, signUpCtrl.user.email, signUpCtrl.user.phone, signUpCtrl.user.favoriteDish);
+            if (signUpCtrl.dishExists()) {
+                signUpCtrl.completed = true;
+            }
         };
 
         signUpCtrl.dishExists = function () {
             console.log("jest");
-            return signUpCtrl.menuService().checkCategory(signUpCtrl.user.favouriteDish).then(function () {
+            return signUpCtrl.menuService().checkCategory(signUpCtrl.user.favoriteDish).then(function () {
+                signUpCtrl.dishExists = true;
                 return true;
             }, function () {
+                signUpCtrl.dishExists = false;
                 return false;
             });
         };
